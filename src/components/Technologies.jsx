f7049a57a7b11de3a9f13872b4fb05fdf3362716
Anuj Dubey <anujd2408@gmail.com>
2025-07-30 23:37:29 +0530

    updated technologies with new design

--- src/components/Technologies.jsx
+++ src/components/Technologies.jsx
@@ -15,6 +15,7 @@ import { FaCloudflare } from "react-icons/fa";
 import { SiPrisma } from "react-icons/si";
 import { SiTurborepo } from "react-icons/si";
 import { motion } from "framer-motion"
+import styled from 'styled-components';
 
 const iconVariants = (duration) => ({
     initial: { y:-10 },
@@ -172,8 +173,147 @@ const Technologies = () =>{
                     <SiThreedotjs  className="text-7xl "/>
                 </motion.div>
             </motion.div>
+
+            <StyledWrapper>
+      <div className="my-loader">
+        <div className="rubiks-cube">
+          <div className="face front">
+            <div style={{background: '#ff3d00'}} className="cube" />
+            <div style={{background: '#ffeb3b'}} className="cube" />
+            <div style={{background: '#4caf50'}} className="cube" />
+            <div style={{background: '#2196f3'}} className="cube" />
+            <div style={{background: '#ffffff'}} className="cube" />
+            <div style={{background: '#ffeb3b'}} className="cube" />
+            <div style={{background: '#4caf50'}} className="cube" />
+            <div style={{background: '#2196f3'}} className="cube" />
+            <div style={{background: '#ff3d00'}} className="cube" />
+          </div>
+          <div className="face back">
+            <div style={{background: '#4caf50'}} className="cube" />
+            <div style={{background: '#ff3d00'}} className="cube" />
+            <div style={{background: '#ffeb3b'}} className="cube" />
+            <div style={{background: '#2196f3'}} className="cube" />
+            <div style={{background: '#ffffff'}} className="cube" />
+            <div style={{background: '#ff3d00'}} className="cube" />
+            <div style={{background: '#ffeb3b'}} className="cube" />
+            <div style={{background: '#4caf50'}} className="cube" />
+            <div style={{background: '#2196f3'}} className="cube" />
+          </div>
+          <div className="face left">
+            <div style={{background: '#ffeb3b'}} className="cube" />
+            <div style={{background: '#4caf50'}} className="cube" />
+            <div style={{background: '#2196f3'}} className="cube" />
+            <div style={{background: '#ff3d00'}} className="cube" />
+            <div style={{background: '#ffffff'}} className="cube" />
+            <div style={{background: '#4caf50'}} className="cube" />
+            <div style={{background: '#2196f3'}} className="cube" />
+            <div style={{background: '#ffeb3b'}} className="cube" />
+            <div style={{background: '#ff3d00'}} className="cube" />
+          </div>
+          <div className="face right">
+            <div style={{background: '#4caf50'}} className="cube" />
+            <div style={{background: '#ff3d00'}} className="cube" />
+            <div style={{background: '#ffeb3b'}} className="cube" />
+            <div style={{background: '#2196f3'}} className="cube" />
+            <div style={{background: '#ffffff'}} className="cube" />
+            <div style={{background: '#ff3d00'}} className="cube" />
+            <div style={{background: '#ffeb3b'}} className="cube" />
+            <div style={{background: '#4caf50'}} className="cube" />
+            <div style={{background: '#2196f3'}} className="cube" />
+          </div>
+          <div className="face top">
+            <div style={{background: '#2196f3'}} className="cube" />
+            <div style={{background: '#ffeb3b'}} className="cube" />
+            <div style={{background: '#ff3d00'}} className="cube" />
+            <div style={{background: '#4caf50'}} className="cube" />
+            <div style={{background: '#ffffff'}} className="cube" />
+            <div style={{background: '#ffeb3b'}} className="cube" />
+            <div style={{background: '#ff3d00'}} className="cube" />
+            <div style={{background: '#4caf50'}} className="cube" />
+            <div style={{background: '#2196f3'}} className="cube" />
+          </div>
+          <div className="face bottom">
+            <div style={{background: '#ffffff'}} className="cube" />
+            <div style={{background: '#4caf50'}} className="cube" />
+            <div style={{background: '#2196f3'}} className="cube" />
+            <div style={{background: '#ff3d00'}} className="cube" />
+            <div style={{background: '#ffeb3b'}} className="cube" />
+            <div style={{background: '#4caf50'}} className="cube" />
+            <div style={{background: '#2196f3'}} className="cube" />
+            <div style={{background: '#ffffff'}} className="cube" />
+            <div style={{background: '#ff3d00'}} className="cube" />
+          </div>
+        </div>
+      </div>
+    </StyledWrapper>
+
         </div>
     )
-}
+};
+
+const StyledWrapper = styled.div`
+
+position: absolute;
+  left: 110px;      
+  top: 1480px; 
+
+  .my-loader {
+    width: 100px;
+    height: 100px;
+    perspective: 500px;
+    margin: 75px auto;
+  }
+
+  .rubiks-cube {
+    width: 100%;
+    height: 100%;
+    position: relative;
+    transform-style: preserve-3d;
+    animation: my-rotateCube 5s infinite linear;
+  }
+
+  .my-loader .face {
+    position: absolute;
+    display: flex;
+    flex-wrap: wrap;
+    width: 100%;
+    height: 100%;
+  }
+
+  .my-loader .face.front {
+    transform: translateZ(50px);
+  }
+  .my-loader .face.back {
+    transform: rotateY(180deg) translateZ(50px);
+  }
+  .my-loader .face.left {
+    transform: rotateY(-90deg) translateZ(50px);
+  }
+  .my-loader .face.right {
+    transform: rotateY(90deg) translateZ(50px);
+  }
+  .my-loader .face.top {
+    transform: rotateX(90deg) translateZ(50px);
+  }
+  .my-loader .face.bottom {
+    transform: rotateX(-90deg) translateZ(50px);
+  }
+
+  .my-loader .cube {
+    width: calc(100% / 3);
+    height: calc(100% / 3);
+    box-sizing: border-box;
+    border: 1px solid #000;
+  }
+
+  @keyframes my-rotateCube {
+    0% {
+      transform: rotateX(0deg) rotateY(0deg);
+    }
+    100% {
+      transform: rotateX(360deg) rotateY(360deg);
+    }
+  }`;
+
 
 export default Technologies
