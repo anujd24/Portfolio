4ac80eb66c3f108e4d3952f2a3b52ec1bea9f313
Anuj Dubey <anujd2408@gmail.com>
2025-08-02 10:51:47 +0530

    added upadated profile pic

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,13 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    rollupOptions :{
+      external : ["styled-components"],
+    }
+  },
+  optimizeDeps :{
+    include : ["styled-components"],
+  },
   plugins: [react()],
 })
